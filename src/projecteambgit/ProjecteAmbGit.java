74cac275604a128f23a014362a1f2852d998d8c9
sebasprofe <sebasprofe17@gmail.com>
2020-12-01 16:31:07 +0100

    Canvis en ProjecteAmbGit, afegisc un for

---
+++ src/projecteambgit/ProjecteAmbGit.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projecteambgit;
+
+/**
+ *
+ * @author sebas
+ */
+public class ProjecteAmbGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mon");
+        int contador=0;//Per anar fent sumes
+        for(int i=0; i<1;i++){
+            contador += i;
+        }
+    }
+    
+}
